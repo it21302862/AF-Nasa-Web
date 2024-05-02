87c8f921d51566d6ffadc0735109eaea6f573480
IT21302862 <sanayasamadhi@gmail.com>
2024-05-02 22:46:03 +0530

    Update SignIn page

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,39 +1,35 @@
-import React, { useEffect } from 'react';
-import GoogleButton from 'react-google-button';
-import { UserAuth } from '../context/AuthContext';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect } from "react";
+import GoogleButton from "react-google-button";
+import { UserAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 const SignIn = () => {
-    const { googleSignIn, user } = UserAuth();
-    const navigate = useNavigate();
-    const handleGoogleSignIn = async () => {
-       try {
-           await googleSignIn();
-
-       } catch (error) {
-           console.error(error);
-       }
-    };
-
-    useEffect(() => {
-        if (user != null) {
-            navigate('/');
-        }
+  const { googleSignIn, user } = UserAuth();
+  const navigate = useNavigate();
+  const handleGoogleSignIn = async () => {
+    try {
+      await googleSignIn();
+    } catch (error) {
+      console.error(error);
     }
-    , [user]);
-    
-    return (
+  };
 
-        
-        <div className='w-[100dvh] h-[90vh] flex items-center justify-center'>
-            <button
+  useEffect(() => {
+    if (user != null) {
+      navigate("/");
+    }
+  }, [user]);
+
+  return (
+    <div className="w-full h-[90vh] flex items-center justify-center">
+      <button
         onClick={handleGoogleSignIn}
         className="bg-black border border-white hover:bg-white hover:text-black font-semibold text-white py-2 px-6 rounded-full"
       >
         Log in with Google
       </button>
-        </div>
-    );
+    </div>
+  );
 };
 
 export default SignIn;
