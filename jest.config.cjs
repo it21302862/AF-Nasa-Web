fb6f16e7f15c304b72c8b4b99d1dc7e27d39e8e0
IT21302862 <sanayasamadhi@gmail.com>
2024-05-04 19:52:33 +0530

    removed all

--- jest.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testEnvironment: "jsdom",
-  preset: 'jest-puppeteer',
-  preset: "ts-jest",
-  transform: {
-    '^.+\\.jsx?$': 'babel-jest',
-  },
-  "testMatch": ["**/__tests__/**/*.js", "**/?(*.)+(spec|test).js"]
-};
